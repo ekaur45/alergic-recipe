be1a43a77aba42e9373069fd5fd44a1eaec6e22a
= <=>
2023-03-11 04:12:59 +0500

    db

--- inc/db/script.sql
+++ inc/db/script.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
--- Host: localhost    Database: recipe-finer
+-- Host: localhost    Database: recipe_new
 -- ------------------------------------------------------
 -- Server version	8.0.28
 
@@ -29,7 +29,7 @@ CREATE TABLE `accounts` (
   `username` varchar(45) DEFAULT NULL,
   `password` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,58 +38,8 @@ CREATE TABLE `accounts` (
 
 LOCK TABLES `accounts` WRITE;
 /*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-INSERT INTO `accounts` VALUES (1,'Waqas','Ahmad','waqas','70b4269b412a8af42b1f7b0d26eceff2');
+INSERT INTO `accounts` VALUES (3,'Waqas','Ahmad','waqas','70b4269b412a8af42b1f7b0d26eceff2');
 /*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `comments`
---
-
-DROP TABLE IF EXISTS `comments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `comments` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `rid` int DEFAULT NULL,
-  `uid` int DEFAULT NULL,
-  `comment` varchar(5000) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `comments`
---
-
-LOCK TABLES `comments` WRITE;
-/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `grocerylist`
---
-
-DROP TABLE IF EXISTS `grocerylist`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `grocerylist` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `ingredientid` int DEFAULT NULL,
-  `userid` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `unique` (`ingredientid`,`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `grocerylist`
---
-
-LOCK TABLES `grocerylist` WRITE;
-/*!40000 ALTER TABLE `grocerylist` DISABLE KEYS */;
-/*!40000 ALTER TABLE `grocerylist` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -101,9 +51,15 @@ DROP TABLE IF EXISTS `ingredients`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ingredients` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `ingredient` varchar(500) DEFAULT NULL,
+  `name` varchar(500) DEFAULT NULL,
+  `energy` float DEFAULT NULL,
+  `fat` float DEFAULT NULL,
+  `unSaturatedFat` float DEFAULT NULL,
+  `carbohydrates` float DEFAULT NULL,
+  `sugar` float DEFAULT NULL,
+  `protein` float DEFAULT NULL,
+  `salt` float DEFAULT NULL,
   `type` varchar(45) DEFAULT NULL,
-  `recipeid` int DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -114,7 +70,7 @@ CREATE TABLE `ingredients` (
 
 LOCK TABLES `ingredients` WRITE;
 /*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
-INSERT INTO `ingredients` VALUES (1,'asdfads','normal',1),(2,'4ads','normal',1),(3,'sdfjiewr','normal',1),(4,'oaoa','normal',1),(5,'sdaouyubkaj','clean',1),(6,'kn ijhdiusa','clean',1),(7,'asdre','clean',1),(8,'ddda','clean',1),(9,'sadfads','normal',2),(10,'sdafads','normal',2),(11,'asdfsda','normal',2),(12,'asdfads','clean',2),(13,'asdfasd','clean',2),(14,'asdf','clean',2);
+INSERT INTO `ingredients` VALUES (1,'gluten',1,2,1,1,1,1,1,'alergic'),(2,'Crustaceans',1,2,1,1,1,1,1,'alergic'),(3,'eggs',1,2,1,1,1,1,1,'alergic'),(4,'Fish',1,2,1,1,1,1,1,'alergic'),(5,'peanuts',1,2,1,1,1,1,1,'alergic'),(6,'soy',1,2,1,1,1,1,1,'alergic'),(7,'milk',1,2,1,1,1,1,1,'alergic'),(8,'Nuts',1,2,1,1,1,1,1,'alergic'),(9,'celery',1,2,1,1,1,1,1,'alergic'),(10,'Mustard',1,2,1,1,1,1,1,'alergic'),(11,'Sesame seeds',1,2,1,1,1,1,1,'alergic'),(12,'Sulfur dioxide and sulphites',1,2,1,1,1,1,1,'alergic'),(13,'sweet lupins',1,2,1,1,1,1,1,'alergic'),(14,'Molluscs',1,2,1,1,1,1,1,'alergic');
 /*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -130,8 +86,9 @@ CREATE TABLE `recipe` (
   `name` varchar(500) DEFAULT NULL,
   `userId` int DEFAULT NULL,
   `file` varchar(500) DEFAULT NULL,
+  `category` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -140,9 +97,37 @@ CREATE TABLE `recipe` (
 
 LOCK TABLES `recipe` WRITE;
 /*!40000 ALTER TABLE `recipe` DISABLE KEYS */;
-INSERT INTO `recipe` VALUES (1,'fadsafsdf',NULL,'uploads/operation-fortune-ruse-de-guerre-2022.jpeg'),(2,'sadfasd',NULL,'uploads/abc.jpg');
+INSERT INTO `recipe` VALUES (4,'New recipe',NULL,'uploads/abc.jpg',NULL);
 /*!40000 ALTER TABLE `recipe` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `recipe_ingredients`
+--
+
+DROP TABLE IF EXISTS `recipe_ingredients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `recipe_ingredients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `recipe_id` int DEFAULT NULL,
+  `ingredient_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recipe_ingredients`
+--
+
+LOCK TABLES `recipe_ingredients` WRITE;
+/*!40000 ALTER TABLE `recipe_ingredients` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recipe_ingredients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'recipe_new'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -153,4 +138,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-07  3:15:57
+-- Dump completed on 2023-03-11  4:12:26
